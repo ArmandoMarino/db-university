233fedc3b2b36e78e9a8a9516b99be34572ee313
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-14 16:43:03 +0100

    7 done

--- queries.sql
+++ queries.sql
@@ -36,7 +36,8 @@ FROM `degrees`
 WHERE `name` LIKE '%magistrale%';
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
-
+SELECT COUNT(`name`) AS 'Numero di Dipartimenti' 
+FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
